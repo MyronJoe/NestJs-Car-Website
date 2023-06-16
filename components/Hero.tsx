92a533c6d7bee38f5481e94696b6aab8cef03fd0
Myron <josephonyekachi77@gmail.com>
2023-06-16 02:20:05 -0800

    updated now

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,17 @@ const Hero = () => {
         </div>
 
         <div className='hero__image-container'>
-            
+            <div className='hero__image'>
+                <Image 
+                    src="/hero.png"
+                    alt='hero-image'
+                    fill
+                    className='object-contain'/>
+
+                <div className='hero__image-overlay'>
+
+                </div>
+            </div>
         </div>
     </div>
   )
