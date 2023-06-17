568d3e55abb6e06ce4c83724f03f28ab30a8519a
Myron <josephonyekachi77@gmail.com>
2023-06-17 02:20:05 -0800

    updated now

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
     }
   return (
     <div className='hero'>
-        <div className='flex-1 pt-10 padding-x'>
+        <div className='flex-1 pt-20 padding-x'>
             <h1 className='hero__title'>
                 Find, Book, or rent a car - quickly and easily!
             </h1>
