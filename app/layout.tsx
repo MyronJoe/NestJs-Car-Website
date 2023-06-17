d8afaa114338b76d8fe5f1315b18b5466e67a535
Myron <josephonyekachi77@gmail.com>
2023-06-17 02:20:05 -0800

    updated now

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
+import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: 'Car Hub',
@@ -17,7 +18,7 @@ export default function RootLayout({
       <body className='relative'>
         <Navbar />
         {children}
-        
+        <Footer />
         </body>
     </html>
   )
