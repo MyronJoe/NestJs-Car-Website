2d9e7e4633ec58655f42000875437bf7b9405f7a
Myron <josephonyekachi77@gmail.com>
2023-06-24 02:20:05 -0800

    updated now

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,21 @@
+"use client"
+
 import React from 'react'
 
+import SearchManufacturer from './searchManufacturer'
+
 const SearchBar = () => {
+    const handleSearch = () => {
+
+    } 
   return (
-    <form action="">
+    <form action="" className='searchbar' onSubmit={handleSearch}>
+
+        <div className='searchbar__item'>
+            <SearchManufacturer 
+            
+            />
+        </div>
         
     </form>
   )
