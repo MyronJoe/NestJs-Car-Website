c66dcb0f1a16b9e6adea77a5dbc86db57e517572
Myron <josephonyekachi77@gmail.com>
2023-06-27 02:20:05 -0800

    updated now

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,6 +1,8 @@
 import { SearchManufacturerProps } from '@/types'
 import React from 'react'
 import { Combobox, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import Image from 'next/image'
 
 const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
   return (
@@ -8,7 +10,22 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
         <Combobox>
 
             <div className='relative w-full'>
+                <Combobox.Button className='absolute top-[14px]'>
+                    <Image
+                        src='/car-logo.svg'
+                        alt='car-logo'
+                        height={20}
+                        width={20}
+                        className='ml-4'
+                    />
+                </Combobox.Button>
 
+                <Combobox.Input
+                    className='search-manufacturer__input'
+                    placeholder='Volkswagen'
+                    displayValue={(manufacturer: string) => manufacturer}
+                    onChange={}
+                />
             </div>
 
         </Combobox>
