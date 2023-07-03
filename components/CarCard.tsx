dc5bdee04982b0d938a0412b8750259937ba0176
Myron <josephonyekachi77@gmail.com>
2023-07-03 02:20:05 -0800

    updated now

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -24,6 +24,18 @@ const CarCard = ({ car }: CarCardProps) => {
                 {carRent}
                 <span className='self-end text-[14px] font-medium'>/day</span>
             </p>
+
+            <div className='relative w-full h-40 my-3 object-contain'>
+                <Image 
+                    src='/hero.png'
+                    alt={`${make} image`}
+                    // width={50}
+                    // height={50}
+                    fill
+                    priority
+                    className='object-contain'
+                />
+            </div>
         </div>
     )
 }
