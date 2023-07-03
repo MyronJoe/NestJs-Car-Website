aaa3b39605dae46825437516e4ff406659192c8e
Myron <josephonyekachi77@gmail.com>
2023-07-03 02:20:05 -0800

    updated now

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,21 +3,29 @@ import React from 'react'
 import Image from 'next/image'
 import { useState } from 'react'
 import CustomButton from './CustomButton'
+import { calculateCarRent } from '@/utils'
 
 interface CarCardProps {
     car: CarProps
 }
 
-const CarCard = ({car}:CarCardProps) => {
-    const {city_mpg, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year } = car
-  return (
-    <div className='car-card group'>
-        <div className='car-card__content'>
-            <h2 className='car-card__content-title'>{make} {model}</h2>
+const CarCard = ({ car }: CarCardProps) => {
+    const { city_mpg, combination_mpg, cylinders, displacement, drive, fuel_type, highway_mpg, make, model, transmission, year } = car
+
+    const carRent = calculateCarRent(city_mpg, year)
+
+    return (
+        <div className='car-card group'>
+            <div className='car-card__content'>
+                <h2 className='car-card__content-title'>{make} {model}</h2>
+            </div>
+            <p className='flex mt-6 text-[32px] font-extrabold'>
+                <span className='self-start text-[14px] font-semibold'>$</span>
+                {carRent}
+                <span className='self-end text-[14px] font-medium'>/day</span>
+            </p>
         </div>
-        <p><span>Car Rent...</span></p>
-    </div>
-  )
+    )
 }
 
 export default CarCard
