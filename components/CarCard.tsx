8b4435999a13b4f8b08df499cf05088282723895
Myron <josephonyekachi77@gmail.com>
2023-07-03 02:20:05 -0800

    updated now

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -26,7 +26,7 @@ const CarCard = ({ car }: CarCardProps) => {
             </p>
 
             <div className='relative w-full h-40 my-3 object-contain'>
-                <Image 
+                <Image
                     src='/hero.png'
                     alt={`${make} image`}
                     // width={50}
@@ -35,6 +35,31 @@ const CarCard = ({ car }: CarCardProps) => {
                     priority
                     className='object-contain'
                 />
+            </div>
+
+            <div className='relative flex w-full mt-2'>
+                <div className='flex group-hover:invisible w-full justify-between text-gray'>
+
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <Image src='/steering-wheel.svg' width={20} height={20} alt='steering' />
+                        <p className='text-[14px]'>
+                            {transmission === 'a' ? 'Automatic' : 'Manual'}
+                        </p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <Image src='/tire.svg' width={20} height={20} alt='tire' />
+                        <p className='text-[14px]'>
+                            {drive.toUpperCase()}
+                        </p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center gap-2'>
+                        <Image src='/gas.svg' width={20} height={20} alt='gas' />
+                        <p className='text-[14px]'>
+                            {city_mpg} MPG
+                        </p>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
