c74f8c1c64c26d0c6f5a4cc4ed844e627542de63
Myron <josephonyekachi77@gmail.com>
2023-07-06 02:20:05 -0800

    updated now

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,4 +1,11 @@
+import { CarProps } from '@/types';
 import React from 'react'
+
+interface CarDetailsProps {
+    isOpen: boolean;
+    closeModal: () => void;
+    car: CarProps;
+}
 
 const CarDetails = () => {
   return (
