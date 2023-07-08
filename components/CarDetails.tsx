ac142bfa999440d360b8ae5274cb4a5d60914caa
Myron <josephonyekachi77@gmail.com>
2023-07-08 02:20:05 -0800

    updated now

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -9,10 +9,65 @@ interface CarDetailsProps {
     car: CarProps;
 }
 
-const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
-  return (
-    <div>CarDetails</div>
-  )
+const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
+    return (
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opacity-25" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+                                <button type='button' onClick={closeModal} className='absolute top-2 left-2 w-fit z-10 p-2 bg-primary-blue-100 rounded-full'>
+                                    <Image 
+                                        src='/close.svg'
+                                        alt='close'
+                                        height={20}
+                                        width={20}
+                                        className='object-contain'
+                                    />
+                                </button>
+
+                 
+                                <div className="flex flex-1 flex-col">
+                            
+                                </div>
+
+                                <div className="mt-4">
+                                    <button
+                                        type="button"
+                                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                        onClick={closeModal}
+                                    >
+                                        Got it, thanks!
+                                    </button>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </div>
+                </div>
+            </Dialog>
+        </Transition>
+    )
 }
 
 export default CarDetails
