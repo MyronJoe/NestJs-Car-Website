5f50cde3f5ebae7ffc91103b7ee4dfb0f5517198
Myron <josephonyekachi77@gmail.com>
2023-07-22 02:20:05 -0800

    updated now

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,5 @@
 export async function getCars() {
-   
+
     const res = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
         headers: {
             'X-RapidAPI-Key': '92a53137f1mshb67166f0ffbacc3p12ab5djsn9d36deb9e5a9',
@@ -16,13 +16,13 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
     const mileageFactor = 0.1; // Additional rate per mile driven
     const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
+
     // Calculate additional rate based on mileage and age
     const mileageRate = city_mpg * mileageFactor;
     const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
+
     // Calculate total rental rate per day
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
+
     return rentalRatePerDay.toFixed(0);
-  };
+};
